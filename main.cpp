347988813af65c2ace0800d2997bf1056928a92c
Michael femi-Lawal <femilawalmicheal@gmail.com>
2023-12-11 03:23:54 -0600

    edited

--- main.cpp
+++ main.cpp
@@ -11,7 +11,6 @@ int main(int argc, char **argv) {
     char command;
     string text;
 
-
     cin >> n;
 
     for (int i = 1; i < n+1; i++) {
@@ -19,87 +18,62 @@ int main(int argc, char **argv) {
         cin >> command;
 
         if (command == 't') {
-
             cin >> text;
             typing.push(text);
-
         }
 
         if (command == 'u') {
-
             if (typing.empty()) {
-
                 cout << "nothing to undo on command " << i << endl;
-
             }
             else {
-
                 undo.push(typing.top());
                 typing.pop();
-
             }
-
         }
 
         if(command == 'r') {
-
             if (undo.empty()) {
-
                 cout << "nothing to redo on command " << i << endl;
-
             }
 
             else {
-
                 typing.push(undo.top());
                 undo.pop();
-
             }
-
         }
 
     }
 
     while(!typing.empty()) {
-
         temp.push(typing.top());
         typing.pop();
-
     }
 
     cout << "Typing Stack : ";
     while(!temp.empty()) {
-
         cout << temp.top();
         temp.pop();
 
         if(!temp.empty()) {
-
             cout << " ";
-
         }
-
     }
     cout << endl;
 
     cout << "Undo Stack   : ";
     while(!undo.empty()) {
-
         temp.push(undo.top());
         undo.pop();
-
     }
 
 
     while(!temp.empty()) {
-
         cout << temp.top();
         temp.pop();
 
         if(!temp.empty()) {
-
             cout << " ";
-
         }
 
     }
